bbc4af63d37c62054bcc8d83d6c6f5055790be9a
Julien Morot <jmorot@gmail.com>
2018-11-14 21:56:01 +0100

    Create influ databases

--- manifests/server.pp
+++ manifests/server.pp
@@ -1,5 +1,7 @@
 class grafanagalera::server (
-
+	$grafana_influxdb_name = "grafanagalera_db",
+	$grafana_influxdb_user = "grafanagalera_user",
+	$grafana_influxdb_pwd = "grafanagalera_pwd",
 ) {
 
 	apt::source { "grafana_deb_repo":
@@ -21,5 +23,27 @@ class grafanagalera::server (
 		enable  => 'true',
 	}
 
+	File { "/root/.${module_name}":
+		ensure => directory,
+		mode   => "750",
+		owner  => root,
+		group  => root,
+	}
+
+	File { "/root/.${module_name}/create_influxdb_res.sh":
+		mode   => "440",
+		owner  => root,
+		group  => root,
+		content => template("${module_name}/create_influxdb_res.sh.erb")
+	}
+
+    Exec { 'exec_create_influxdb_res':
+        command  => "/bin/bash /root/.${module_name}/create_influxdb_res.sh && touch /root/.${module_name}/create_influxdb_res.sh.done",
+        cwd      => "/root",
+        path     => '/usr/bin:/usr/sbin:/bin:/sbin',
+        provider => shell,
+        unless   => ["test -f /root/.${module_name}/create_influxdb_res.sh.done"],
+        require  => File["/root/.${module_name}/create_influxdb_res.sh"],
+    }
 
 }
