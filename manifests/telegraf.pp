ae52a4c666c97dd6390cbc310f41a817eaaeee88
Julien Morot <jmorot@gmail.com>
2018-12-16 22:22:07 +0100

    Update telegraf config

--- manifests/telegraf.pp
+++ manifests/telegraf.pp
@@ -1,13 +1,25 @@
 class grafanagalera::telegraf (
+    $grafana_influxdb_name = "grafanagalera_db",
+    $grafana_influxdb_user = "grafanagalera_user",
+    $grafana_influxdb_pwd = "grafanagalera_pwd",
+    $grafana_influxdb_server = "",
 ) {
 
     include ::grafanagalera
-	ensure_packages(['telegraf'], { ensure => present, })
+    ensure_packages(['telegraf'], { ensure => present, })
 
-	Service { 'telegraf':
-		ensure  => 'running',
-		enable  => 'true',
-	}
+    File { "/etc/telegraf/telegraf.conf":
+        mode   => "644",
+        owner  => root,
+        group  => root,
+        content => template("${module_name}/telegraf.conf.erb"),
+        notify => Service['telegraf'],
+    }
+
+    Service { 'telegraf':
+        ensure  => 'running',
+        enable  => 'true',
+    }
 
 
 }
